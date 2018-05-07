b9db61fbcdd83982e4fc44679b4776a1a33bffe9
jie Hao <jiehao@jies-MacBook-Pro.local>
2018-05-07 18:28:28 -0400

    Create new file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // Create a function that creates the start button and initial screen
+
     
     function initialScreen() {
         startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
@@ -8,60 +8,57 @@ $(document).ready(function() {
     
     initialScreen();
     
-    //Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
-    
     $("body").on("click", ".start-button", function(event){
-        event.preventDefault();  // added line to test issue on GitHub Viewer
+        event.preventDefault();  
         clickSound.play();
         generateHTML();
     
         timerWrapper();
     
-    }); // Closes start-button click
+    }); 
     
     $("body").on("click", ".answer", function(event){
-        //answeredQuestion = true;
+       
         clickSound.play();
         selectedAnswer = $(this).text();
         if(selectedAnswer === correctAnswers[questionCounter]) {
-            //alert("correct");
-    
+            
             clearInterval(theClock);
             generateWin();
         }
         else {
-            //alert("wrong answer!");
+            
             clearInterval(theClock);
             generateLoss();
         }
-    }); // Close .answer click
+    }); 
     
     $("body").on("click", ".reset-button", function(event){
         clickSound.play();
         resetGame();
-    }); // Closes reset-button click
+    }); 
     
-    });  //  Closes jQuery wrapper
+    });  
     
     function generateLossDueToTimeOut() {
         unansweredTally++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>";
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
+        setTimeout(wait, 2000);  
     }
     
     function generateWin() {
         correctTally++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter];
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
+        setTimeout(wait, 2000); 
     }
     
     function generateLoss() {
         incorrectTally++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter];
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000); //  change to 4000 or other amount
+        setTimeout(wait, 2000); 
     }
     
     function generateHTML() {
